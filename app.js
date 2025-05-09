24b02e1ad3d3b3e93cba1fdfcd83ac08d0b8c2e2
Abhinav Garg <abhichhrp@gmail.com>
2025-05-09 23:53:02 +0530

    feat(health): add basic health check endpoint

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const healthRoute = require('./routes/healthCheck')
 
 app.use(express.json());
+app.use('/health',healthRoute)
 
 module.exports = app;
